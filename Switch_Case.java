c55dd2f3e11b0f669beff9799f368defd26adc44
unknown <poojagadhewal2006gmail.com>
2025-04-11 10:10:59 +0530

    hello

---
+++ Switch_Case.java
@@ -0,0 +1,20 @@
+public class Switch_Case {
+    public static void main(String[] args) {
+        int a=8;
+        switch(a)
+        {
+            case 1 : 
+            case 2 :
+            case 3 :
+            case 4 :
+            System.out.println("hello");
+            break;
+            case 5 :
+            System.out.println("hii");
+            break;
+            default :
+            System.out.println("byee"); 
+            break;
+        }
+    }
+}
