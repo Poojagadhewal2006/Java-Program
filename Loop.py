e8bda2f87ac393fe9b1b23a57c761a737d8aaafc
unknown <poojagadhewal2006gmail.com>
2025-06-01 12:47:55 +0530

    Python

---
+++ Loop.py
@@ -0,0 +1,13 @@
+n=4
+for i in range(0,n):
+    print(i)
+
+#sum =0
+#for i in range(1,11):
+  #  sum+=i
+   # print("The sum of first 10 natural number is :",sum)
+    
+#num = int(input("Enter the number : "))
+#print("Multiplication table of n :")
+#for i in range(1,11):
+ #   print(num*i)
---
+++ Practice.py
@@ -0,0 +1,45 @@
+n=4
+for i in range(0,n):
+    print(i)
+    #n=4
+    #for i in range(0,n) :
+     #  print(i)
+
+   # for iterator in sequence(start, stop, step) (1,10,2)
+    #System.err.println();
+
+    #my_range=10
+   # for i in range(my_range):
+   # if(i%2==0):   # if the number is even or not
+   # print(i)
+
+    #n=1
+   # while(n<10):
+   # print("Hello")
+   # n+=1
+     
+   # amount =100
+   # while(amount>0):
+   # spent = int(input("enter the amount you spent:"))
+   # amount -=spent
+   # print(amount)
+
+    # amount =100
+   # while(amount>0):
+   # spent = int(input("enter the amount you spent:"))
+   # if(spent<=amount):
+   # amount -=spent
+   # print(amount)
+   # else:
+  #  print("you dont have enough amount:")
+    #print("you have reached your limit:")
+
+   # string ="pooja"
+  #  for i in string():       #Error
+   # for i in "pooja0"
+   # print(i)
+
+   # do{
+       # print()
+   # }while(n>10):
+   
